f5fb983f8464fd444e51273e84dd6fca5c4872a3
Tomasz Wasilczyk <twasilczyk@google.com>
2024-07-25 10:07:05 -0700

    Avoid crash when checking if EuiccManager is enabled
    Bug: 347624913
    Test: android.carrierapi.cts.CarrierApiTest#testAddSubscriptionToExistingGroupForEsim
    Change-Id: I918f6562949e32d3581bf0a69c9bfe02dce1cd3b

--- src/java/com/android/internal/telephony/subscription/SubscriptionManagerService.java
+++ src/java/com/android/internal/telephony/subscription/SubscriptionManagerService.java
@@ -2157,7 +2157,7 @@ public class SubscriptionManagerService extends ISub.Stub {
     @Override
     public List<SubscriptionInfo> getAccessibleSubscriptionInfoList(
             @NonNull String callingPackage) {
-        if (!mEuiccManager.isEnabled()) {
+        if (mEuiccManager == null || !mEuiccManager.isEnabled()) {
             return null;
         }
 
